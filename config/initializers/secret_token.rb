abd731e7883a448a5998897be6d76086133faf94
Rambabu <ram@ubuntu.(none)>
2013-08-11 17:52:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitL::Application.config.secret_token = 'd9e33819a8e172dfab15c0d7ec9914ab75384d4b1faa17e9b599b54d9b7d56ff4c7d7b493ddc0ba5f5290247041dd092197210c289ce17d9c748a2fe8e577c2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GitL::Application.config.session_store :cookie_store, key: '_gitL_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GitL::Application.config.session_store :active_record_store
